9ac939f8c9a3026b561648ac16b20500a065016b
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2019-06-03 18:21:54 +0200

    Aún queda configurar los datos del xml del cliente

--- uaclient.py
+++ uaclient.py
@@ -11,7 +11,7 @@ import threading
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-
+ 
 class UAclienthandler(ContentHandler):
     """Clase """
 
@@ -39,17 +39,27 @@ class UAclienthandler(ContentHandler):
 
 
 
+if __name__ == "__main__":
 
+    if len(sys.argv) != 4:
+       sys.exit('Usage: python3 uaclient.py config method option')
 
+    CONFIGU = sys.argv[1]
+    if not os.path.exists(CONFIGU):
+        sys.exit('Config does not exists')
 
+    METHOD = sys.argv[2]
+    LIST = ['REGISTER', 'INVITE', 'BYE']
+    if METHOD not in LIST:
+        sys.exit('SIP/2.0 405 Method Not Allowed\r\n')
 
+    OPTION = sys.argv[3]
 
-
-
-
-
-
-
-
-
-
+    # sacamos el xml
+    parser = make_parser()
+    xmlHandler = UAclienthandler()
+    parser.setContentHandler(xmlHandler)
+    parser.parse(open(CONFIGU))
+    XML_List = xmlHandler.get_tags()
+  
+ 
