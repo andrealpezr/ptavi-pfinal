f8bea3238c5f170690c51332313940bd25586dd9
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2019-06-28 16:32:54 +0200

    servidor

--- uaserver.py
+++ uaserver.py
@@ -15,84 +15,81 @@ class XMLServer(ContentHandler):
 
     def __init__(self):
         """Inicializo las variabes."""
-        self.Lista_server = []
+        self.Lista_serv = []
 
     def startElement(self, element, attrs):
-        """Crea las etiquetas y tributos del xml."""
-        self.dicc_etiq_server = {'account': ['username', 'passwd'],
-                                 'uaserver': ['ip', 'puerto'],
-                                 'rtpaudio': ['puerto'],
-                                 'regproxy': ['ip', 'puerto'],
-                                 'log': ['path'],
-                                 'audio': ['path']
-                                 }
-        if element in self.dicc_etiq_server:
-            Dict_server = {}
-            for atrib in self.dicc_etiq_server[element]:
-                Dict_server[atrib] = attrs.get(atrib, "")
-            self.Lista_server.append([element, Dict_server])
+        """Crea las etiquetas y tributos del xml. Busca nombres, no guarda."""
+        self.dicc_etiq = {'account': ['username', 'passwd'],
+                          'uaserver': ['ip', 'puerto'],
+                          'rtpaudio': ['puerto'],
+                          'regproxy': ['ip', 'puerto'],
+                          'log': ['path'],
+                          'audio': ['path']}
+        if element in self.dicc_etiq:
+            Dict = {}
+            # Recorre los atributos y los guarda en Dict
+            for atrib in self.dicc_etiq[element]:  # Busco en etiquetas=element
+                Dict[atrib] = attrs.get(atrib, "")
+            # Guarda sin sustituir lo que habia dentro
+            self.Lista_serv.append([element, Dict])
 
     def get_tags(self):
         """Devuelve los datos del xml del cliente."""
-        return self.Lista_server
+        return self.Lista_serv
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """Client handler requests."""
 
     USER = []
+    PORT = []
 
     def handle(self):
         """Escribe direccion y puerto del cliente."""
-        while 1:
-            # Lee linea a linea lo que recibe del cliente
-            line = self.rfile.read()
-            lines = line.decode('utf-8').split(' ')
-            # Si hay linea en blanco sale del bucle
-            if not lines:
-                break
-            reciv = ' '.join(lines)
-            METOD = lines[0]
-            if METOD == 'INVITE' or METOD == 'invite':
-                # Lo envio tras recibir el INVITE del Proxy
-                SEND = 'SIP/2.0 100 Trying...' + 'r\n\r\n'
-                SEND += 'SIP/2.0 180 Ringing' + 'r\n\r\n'
-                ok = 'SIP/2.0 200 OK' + '\r\n'
-                sdp = ok
-                sdp += 'Content-Type: application/sdp \r\n\r\n' + 'v=0 \r\n'
-                sdp += "o=" + USERNAME + " " + UASERV_IP + '\r\n'
-                sdp += 's=lasesion \r\n' + 't=0\r\n' + 'm=audio '
-                sdp += RTP_PORT + ' RTP \r\n'
-                # Envio peticion
-                ALL = (SEND + sdp)
-                print(ALL)
-                self.wfile.write(bytes(ALL, 'utf-8'))
-                IP_CLIENT = reciv.split('o=')[1].split(' ')[1].split('\r')[0]
-                AUDIO_CLI = reciv.split('m=')[1].split(' ')[1].split(' R')[0]
-                self.USER.append(IP_CLIENT)
-                self.USER.append(AUDIO_CLI)
-            if 'ACK' in lines:
-                # Tras recibir el ack mando el rtp
-                print(self.USER)
-                print('He recibido el ACK, envio RTP')
-                aEjecutar = './mp32rtp -i ' + self.USER[0] + " -p "
-                aEjecutar += self.USER[1] + ' < ' + AUDIO_PATH
-                print('Vamos a ejecutar ', aEjecutar)
-                os.system(aEjecutar)
-                print('La ejecucion  RTP ha finalizado \r\n')
+        # Lee linea a linea lo que recibe del cliente
+        line = self.rfile.read()
+        recibo = line.decode('utf-8')
+        lines = recibo.split()
+        print('Recibo del Proxy...' + 'r\n' + recibo)
+        METOD = lines[0]
+        if METOD == 'INVITE' or METOD == 'invite':
+            self.USER.append(lines[7])
+            self.PORT.append(lines[11])
+            # Lo envio tras recibir el INVITE del Proxy
+            SEND = 'SIP/2.0 100 Trying' + 'r\n\r\n'
+            SEND += 'SIP/2.0 180 Ringing' + 'r\n\r\n'
+            ok = 'SIP/2.0 200 OK' + '\r\n'
+            sdp = ok
+            sdp += 'Content-Type: application/sdp \r\n\r\n' + 'v=0 \r\n'
+            sdp += "o=" + USERNAME + " " + UASERV_IP + '\r\n'
+            sdp += 's=lasesion \r\n' + 't=0\r\n' + 'm=audio '
+            sdp += RTP_PORT + ' RTP \r\n'
+            # Envio peticion
+            ALL = (SEND + sdp)
+            self.wfile.write(bytes(ALL, 'utf-8') + b'\r\n')
+            print('Enviamos al cliente...' + '\r\n' + ALL)
+        elif METOD == 'ACK' or METOD == 'ack':
+            # Tras recibir el ack mando el rtp
+            print('He recibido el ACK, envio RTP')
+            aEjecutar = './mp32rtp -i ' + UASERV_IP + " -p "
+            aEjecutar += RTP_PORT + ' < ' + AUDIO_PATH
+            print('Vamos a ejecutar ', aEjecutar)
+            os.system(aEjecutar)
+            print('La ejecucion  RTP ha finalizado')
             # En caso de recibir un BYE
-            if METOD == 'BYE' or METOD == 'bye':
-                print('Mando al proxy:\r\n SIP/2.0 200 OK\r\n\r\n')
-                self.wfile.write(b'SIP/2.0 200 OK'+b'\r\n\r\n')
-                break
+        elif METOD == 'BYE' or METOD == 'bye':
+            ok = 'SIP/2.0 200 OK\r\n'
+            self.wfile.write(bytes(ok, 'utf-8') + b'\r\n')
+            print('Enviamos al Proxy...' + '\r\n' + ok)
             # En caso de introducir mal el metodo
-            if METOD not in ['REGISTER', 'INVITE', 'BYE', 'ACK']:
-                sys.exit('SIP/2.0 405 Method Not Allowed\r\n')
-            if ConnectionRefusedError:
-                self.wfile.write(b'SIP/2.0 Connection Refused'+b'\r\n\r\n')
-                break
-            else:
-                self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
+        elif METOD not in ['REGISTER', 'INVITE', 'BYE', 'ACK']:
+            mal_metodo = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
+            self.wfile.write(bytes(mal_metodo))
+        elif ConnectionRefusedError:
+            self.wfile.write(b'SIP/2.0 Connection Refused' + b'\r\n\r\n')
+            sys.exit()
+        else:
+            self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
 
 
 if __name__ == "__main__":
@@ -120,7 +117,7 @@ if __name__ == "__main__":
 
         SERV = socketserver.UDPServer((UASERV_IP, int(UASERV_PORT)),
                                       EchoHandler)
-        print('Listening...\r\n')
+        print('Listening...\n')
         try:
             SERV.serve_forever()
         except FileNotFoundError:
