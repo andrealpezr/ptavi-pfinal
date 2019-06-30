c5464854aa7b681e318ea89e08e8b2a852ee2b5d
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2019-06-30 18:39:32 +0200

    corregido peps

--- uaserver.py
+++ uaserver.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import time
 import socketserver
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
@@ -38,6 +39,13 @@ class XMLServer(ContentHandler):
         return self.Lista_serv
 
 
+def LOG(EVENT):
+    """Escribe en el fichero Log."""
+    hora = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())) + ' '
+    file = open(log_path, 'a+')  # Abro fichero
+    file.write(str(hora) + ' ' + EVENT + '\r\n')  # Escribo en el fichero
+
+
 class EchoHandler(socketserver.DatagramRequestHandler):
     """Client handler requests."""
 
@@ -50,25 +58,32 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         line = self.rfile.read()
         recibo = line.decode('utf-8')
         lines = recibo.split()
-        print('Recibo del Proxy...' + 'r\n' + recibo)
+        print('Recibo del Proxy...' + '\r\n' + recibo)
+        # Cojo la IP y puerto del cliente
+        IP_CL = str(self.client_address[0])
+        PORT_CL = str(self.client_address[1])
+        # Escribo en el fichero Log el mensaje recibido
+        LOG(log_path, 'Recibo de' + IP_CL + ':' + PORT_CL + ':' + recibo)
         METOD = lines[0]
         if METOD == 'INVITE' or METOD == 'invite':
             self.USER.append(lines[7])
             self.PORT.append(lines[11])
             # Lo envio tras recibir el INVITE del Proxy
-            SEND = 'SIP/2.0 100 Trying' + 'r\n\r\n'
-            SEND += 'SIP/2.0 180 Ringing' + 'r\n\r\n'
-            ok = 'SIP/2.0 200 OK' + '\r\n'
-            sdp = ok
-            sdp += 'Content-Type: application/sdp \r\n\r\n' + 'v=0 \r\n'
+            SEND = 'SIP/2.0 100 Trying\r\n\r\n'
+            SEND += 'SIP/2.0 180 Ringing\r\n\r\n'
+            SEND += 'SIP/2.0 200 OK\r\n\r\n'
+            sdp = 'Content-Type: application/sdp\r\n\r\n' + 'v=0 \r\n'
             sdp += "o=" + USERNAME + " " + UASERV_IP + '\r\n'
-            sdp += 's=lasesion \r\n' + 't=0\r\n' + 'm=audio '
-            sdp += RTP_PORT + ' RTP \r\n'
-            # Envio peticion
-            ALL = (SEND + sdp)
-            self.wfile.write(bytes(ALL, 'utf-8') + b'\r\n')
-            print('Enviamos al cliente...' + '\r\n' + ALL)
-        elif METOD == 'ACK' or METOD == 'ack':
+            sdp += 's=lasesion\r\n' + 't=0\r\n' + 'm=audio '
+            sdp += RTP_PORT + ' RTP\r\n\r\n'
+            ALL = SEND + sdp
+            #  Envio al Proxy
+            self.wfile.write(bytes(ALL, 'utf-8'))
+            print('Enviamos al Proxy...' + '\r\n' + ALL)
+            # Escribo en el LOG el mensaje a enviar
+            enviar = 'Envio a ' + IP_CL + ':' + PORT_CL + ':' + ALL
+            LOG(enviar)
+        elif METOD == 'ACK' or METOD == 'ACK':
             # Tras recibir el ack mando el rtp
             print('He recibido el ACK, envio RTP')
             aEjecutar = './mp32rtp -i ' + UASERV_IP + " -p "
@@ -76,20 +91,39 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             print('Vamos a ejecutar ', aEjecutar)
             os.system(aEjecutar)
             print('La ejecucion  RTP ha finalizado')
+            # Escribo en el LOG el RTP
+            ejec = 'Vamos a ejecutar RTP a ' + IP_CL + ':' + PORT_CL
+            LOG(ejec)
             # En caso de recibir un BYE
         elif METOD == 'BYE' or METOD == 'bye':
-            ok = 'SIP/2.0 200 OK\r\n'
-            self.wfile.write(bytes(ok, 'utf-8') + b'\r\n')
+            ok = 'SIP/2.0 200 OK\r\n\r\n'
+            self.wfile.write(bytes(ok, 'utf-8'))
             print('Enviamos al Proxy...' + '\r\n' + ok)
-            # En caso de introducir mal el metodo
+            # Escribo en el LOG el OK
+            recibo = ('Recibo de ' + IP_CL + ':' + PORT_CL + ':' +
+                      'SIP/2.0 200 OK')
+            LOG(recibo)
+        # En caso de introducir mal el metodo
         elif METOD not in ['REGISTER', 'INVITE', 'BYE', 'ACK']:
             mal_metodo = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
             self.wfile.write(bytes(mal_metodo))
+            # Escribo en el LOG el mensaje de metodo mal formulado
+            recibo = ('Recibo de ' + IP_CL + ':' + PORT_CL + ':' +
+                      'SIP/2.0 200 OK')
+            LOG(recibo)
         elif ConnectionRefusedError:
             self.wfile.write(b'SIP/2.0 Connection Refused' + b'\r\n\r\n')
             sys.exit()
+            # Escribo en el LOG el fallo de conexion
+            recibo = ('Recibo de ' + IP_CL + ':' + PORT_CL + ':' +
+                      'SIP/2.0 Connection Refused')
+            LOG(recibo)
         else:
             self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
+            # Escribo en el LOG el error de pregunta
+            request = ('Recibo de ' + IP_CL + ':' + PORT_CL + ':' +
+                       'SIP/2.0 400 Bad Request')
+            LOG(request)
 
 
 if __name__ == "__main__":
@@ -113,7 +147,6 @@ if __name__ == "__main__":
         PROX_PORT = datos_XML[3][1]['puerto']  # Es el puerto del PROXY
         log_path = datos_XML[4][1]['path']  # Es el fichero log
         AUDIO_PATH = datos_XML[5][1]['path']  # Es el audio log
-        # Defino el log para poner escribir en el fichero
 
         SERV = socketserver.UDPServer((UASERV_IP, int(UASERV_PORT)),
                                       EchoHandler)
