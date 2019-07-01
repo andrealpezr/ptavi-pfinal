fa443a14cf6c45241daca560ba893eb15228b74d
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2019-07-01 03:02:17 +0200

    pasado peps

--- uaserver.py
+++ uaserver.py
@@ -11,43 +11,38 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
-class XMLServer(ContentHandler):
-    """Clase que extrae e imprime el xml del servidor."""
+class XMLSERVER(ContentHandler):
+    """Clase que extrae e imprime el xml del cliente."""
 
     def __init__(self):
-        """Inicializo las variabes."""
-        self.Lista_serv = []
+        """Inicializa los diccionarios."""
+        self.diccionario = {}
+        self.dicc_ua1xml = {'account': ['username', 'passwd'],
+                            'uaserver': ['ip', 'puerto'],
+                            'rtpaudio': ['puerto'],
+                            'regproxy': ['ip', 'puerto'],
+                            'log': ['path'], 'audio': ['path']}
 
-    def startElement(self, element, attrs):
-        """Crea las etiquetas y tributos del xml. Busca nombres, no guarda."""
-        self.dicc_etiq = {'account': ['username', 'passwd'],
-                          'uaserver': ['ip', 'puerto'],
-                          'rtpaudio': ['puerto'],
-                          'regproxy': ['ip', 'puerto'],
-                          'log': ['path'],
-                          'audio': ['path']}
-        if element in self.dicc_etiq:
-            Dict = {}
-            # Recorre los atributos y los guarda en Dict
-            for atrib in self.dicc_etiq[element]:  # Busco en etiquetas=element
-                Dict[atrib] = attrs.get(atrib, "")
-            # Guarda sin sustituir lo que habia dentro
-            self.Lista_serv.append([element, Dict])
+    def startElement(self, name, attrs):
+        """Crea el diccionario con los valores del fichero xml."""
+        if name in self.dicc_ua1xml:
+            for atributo in self.dicc_ua1xml[name]:
+                self.diccionario[name+'_'+atributo] = attrs.get(atributo, '')
 
     def get_tags(self):
-        """Devuelve los datos del xml del cliente."""
-        return self.Lista_serv
+        """Devuelve el diccionario creado."""
+        return self.diccionario
 
 
-def LOG(EVENT):
+def writelog(eventofile):
     """Escribe en el fichero Log."""
     hora = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())) + ' '
-    file = open(log_path, 'a+')  # Abro fichero
-    file.write(str(hora) + ' ' + EVENT + '\r\n')  # Escribo en el fichero
+    filelog = open(LOG_PATH, 'a+')  # Abro fichero
+    filelog.write(str(hora) + ' ' + eventofile + '\r\n')  # Escribo fichero
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
-    """Client handler requests."""
+    """Metodo del servidor que establece conexion con el cliente y proxy."""
 
     USER = []
     PORT = []
@@ -55,106 +50,109 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         """Escribe direccion y puerto del cliente."""
         # Lee linea a linea lo que recibe del cliente
-        line = self.rfile.read()
-        recibo = line.decode('utf-8')
+        recibo = self.rfile.read().decode('utf-8')
         lines = recibo.split()
         print('Recibo del Proxy...' + '\r\n' + recibo)
         # Cojo la IP y puerto del cliente
-        IP_CL = str(self.client_address[0])
-        PORT_CL = str(self.client_address[1])
+        ipcliente = str(self.client_address[0])
+        portcliente = str(self.client_address[1])
         # Escribo en el fichero Log el mensaje recibido
-        LOG(log_path, 'Recibo de' + IP_CL + ':' + PORT_CL + ':' + recibo)
-        METOD = lines[0]
-        if METOD == 'INVITE' or METOD == 'invite':
+        mens = 'Recibo de ' + ipcliente + ':' + portcliente + ':' + recibo
+        writelog(mens)
+        metodo = lines[0]
+        if metodo == 'INVITE' or metodo == 'invite':
             self.USER.append(lines[7])
             self.PORT.append(lines[11])
             # Lo envio tras recibir el INVITE del Proxy
-            SEND = 'SIP/2.0 100 Trying\r\n\r\n'
-            SEND += 'SIP/2.0 180 Ringing\r\n\r\n'
-            SEND += 'SIP/2.0 200 OK\r\n\r\n'
+            enviando = 'SIP/2.0 100 Trying\r\n\r\n'
+            enviando += 'SIP/2.0 180 Ringing\r\n\r\n'
+            enviando += 'SIP/2.0 200 OK\r\n\r\n'
             sdp = 'Content-Type: application/sdp\r\n\r\n' + 'v=0 \r\n'
             sdp += "o=" + USERNAME + " " + UASERV_IP + '\r\n'
             sdp += 's=lasesion\r\n' + 't=0\r\n' + 'm=audio '
-            sdp += RTP_PORT + ' RTP\r\n\r\n'
-            ALL = SEND + sdp
+            sdp += str(RTP_PORT) + ' RTP\r\n\r\n'
+            todo = enviando + sdp
             #  Envio al Proxy
-            self.wfile.write(bytes(ALL, 'utf-8'))
-            print('Enviamos al Proxy...' + '\r\n' + ALL)
+            self.wfile.write(bytes(todo, 'utf-8'))
+            print('Enviamos al Proxy...' + '\r\n' + todo)
             # Escribo en el LOG el mensaje a enviar
-            enviar = 'Envio a ' + IP_CL + ':' + PORT_CL + ':' + ALL
-            LOG(enviar)
-        elif METOD == 'ACK' or METOD == 'ACK':
+            enviar = 'Envio a ' + ipcliente + ':' + portcliente + ':' + todo
+            writelog(enviar)
+        elif metodo == 'ACK' or metodo == 'ACK':
             # Tras recibir el ack mando el rtp
             print('He recibido el ACK, envio RTP')
-            aEjecutar = './mp32rtp -i ' + UASERV_IP + " -p "
-            aEjecutar += RTP_PORT + ' < ' + AUDIO_PATH
-            print('Vamos a ejecutar ', aEjecutar)
-            os.system(aEjecutar)
+            aejecutar = './mp32rtp -i ' + UASERV_IP + " -p "
+            aejecutar += str(RTP_PORT) + ' < ' + AUDIO_PATH
+            print('Vamos a ejecutar ' + aejecutar)
+            os.system(aejecutar)
             print('La ejecucion  RTP ha finalizado')
             # Escribo en el LOG el RTP
-            ejec = 'Vamos a ejecutar RTP a ' + IP_CL + ':' + PORT_CL
-            LOG(ejec)
+            ejec = 'Vamos a ejecutar RTP a ' + ipcliente + ':' + portcliente
+            writelog(ejec)
             # En caso de recibir un BYE
-        elif METOD == 'BYE' or METOD == 'bye':
-            ok = 'SIP/2.0 200 OK\r\n\r\n'
-            self.wfile.write(bytes(ok, 'utf-8'))
-            print('Enviamos al Proxy...' + '\r\n' + ok)
+        elif metodo == 'BYE' or metodo == 'bye':
+            okmens = 'SIP/2.0 200 OK\r\n\r\n'
+            self.wfile.write(bytes(okmens, 'utf-8'))
+            print('Enviamos al Proxy...' + '\r\n' + okmens)
             # Escribo en el LOG el OK
-            recibo = ('Recibo de ' + IP_CL + ':' + PORT_CL + ':' +
+            recibo = ('Recibo de ' + ipcliente + ':' + portcliente + ':' +
                       'SIP/2.0 200 OK')
-            LOG(recibo)
+            writelog(recibo)
         # En caso de introducir mal el metodo
-        elif METOD not in ['REGISTER', 'INVITE', 'BYE', 'ACK']:
+        elif metodo not in ['REGISTER', 'INVITE', 'BYE']:
             mal_metodo = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
             self.wfile.write(bytes(mal_metodo))
             # Escribo en el LOG el mensaje de metodo mal formulado
-            recibo = ('Recibo de ' + IP_CL + ':' + PORT_CL + ':' +
+            recibo = ('Recibo de ' + ipcliente + ':' + portcliente + ':' +
                       'SIP/2.0 200 OK')
-            LOG(recibo)
-        elif ConnectionRefusedError:
-            self.wfile.write(b'SIP/2.0 Connection Refused' + b'\r\n\r\n')
-            sys.exit()
-            # Escribo en el LOG el fallo de conexion
-            recibo = ('Recibo de ' + IP_CL + ':' + PORT_CL + ':' +
-                      'SIP/2.0 Connection Refused')
-            LOG(recibo)
+            writelog(recibo)
         else:
             self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
             # Escribo en el LOG el error de pregunta
-            request = ('Recibo de ' + IP_CL + ':' + PORT_CL + ':' +
+            request = ('Recibo de ' + ipcliente + ':' + portcliente + ':' +
                        'SIP/2.0 400 Bad Request')
-            LOG(request)
+            writelog(request)
 
 
 if __name__ == "__main__":
 
     if len(sys.argv) == 2:
-        CONFIG = sys.argv[1]
+        try:
+            CONFIG = sys.argv[1]
+        except IndexError:
+            sys.exit("Usage: python3 uaserver.py config")
         # Creo el socket para parsear el XML y trabajar con el
-        parser = make_parser()
-        Handler = XMLServer()
-        parser.setContentHandler(Handler)
-        parser.parse(open(CONFIG))
-        datos_XML = Handler.get_tags()
+        PARSER = make_parser()
+        U2HANDLER = XMLSERVER()
+        PARSER.setContentHandler(U2HANDLER)
+        try:
+            PARSER.parse(open(CONFIG))
+        except(IndexError, ValueError):
+            sys.exit('Usage: python3 uaserver.py config')
+        DATOS_XML = U2HANDLER.get_tags()
 
         # Extraigo el fichero XML
-        USERNAME = datos_XML[0][1]['username']  # Es el nombre SIP
-        USER_PASS = datos_XML[0][1]['passwd']  # Es la contraseña SIP
-        UASERV_IP = datos_XML[1][1]['ip']  # Es el ip del servidor
-        UASERV_PORT = datos_XML[1][1]['puerto']  # Es el servidor del servidor
-        RTP_PORT = datos_XML[2][1]['puerto']  # Es el puerto del RTP
-        PROXY_IP = datos_XML[3][1]['ip']  # Es la IP del PROXY
-        PROX_PORT = datos_XML[3][1]['puerto']  # Es el puerto del PROXY
-        log_path = datos_XML[4][1]['path']  # Es el fichero log
-        AUDIO_PATH = datos_XML[5][1]['path']  # Es el audio log
+        USERNAME = DATOS_XML['account_username']  # Es el nombre SIP
+        RTP_PORT = int(DATOS_XML['rtpaudio_puerto'])  # Es el puerto del RTP
+        PROX_PORT = int(DATOS_XML['regproxy_puerto'])  # Es el puerto del PROXY
+        LOG_PATH = DATOS_XML['log_path']  # Es el fichero log
+        AUDIO_PATH = DATOS_XML['audio_path']  # Es el audio log
+        UASERV_PORT = DATOS_XML['uaserver_puerto']  # Es el port del servidor
+        if DATOS_XML['uaserver_ip'] == '':  # En caso de no tener IP el servido
+            UASERV_IP = '127.0.0.1'  # Por defecto es 127.0.0.1
+        else:
+            UASERV_IP = DATOS_XML['uaserver_ip']  # Si no esta vacia, es xml
 
-        SERV = socketserver.UDPServer((UASERV_IP, int(UASERV_PORT)),
-                                      EchoHandler)
-        print('Listening...\n')
+        if DATOS_XML['regproxy_ip'] == '':  # En caso de no tener IP el Proxy
+            PROXY_IP = '127.0.0.1'  # Por defecto es 127.0.0.1
+        else:
+            PROXY_IP = DATOS_XML['regproxy_ip']  # Si no esta vacia, es ip xml
+
+        SERVI = socketserver.UDPServer((UASERV_IP, int(UASERV_PORT)),
+                                       EchoHandler)
+        print('Listening...\r\n')
         try:
-            SERV.serve_forever()
-        except FileNotFoundError:
-            sys.exit('File not found!')
+            SERVI.serve_forever()
         except IndexError:
             sys.exit("Usage: python3 uaserver.py config")
         except KeyboardInterrupt:
